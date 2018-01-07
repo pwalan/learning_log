fcbcb708feefc454d80aa38ac8e00899c5b938c6
alanp <pwalan@foxmail.com>
2018-01-07 11:38:57 +0800

    add index and topics view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
+
+
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """显示所有主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
